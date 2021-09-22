d86459ddc7c325fa0c9c35510e5a5fcd3c5f770d
Bilal Kabas <bilalkabas@icloud.com>
2021-09-22 23:12:20 +0300

    Update airsim_env.py

--- scripts/airsim_env.py
+++ scripts/airsim_env.py
@@ -165,17 +165,36 @@ class AirSimDroneEnv(gym.Env):
 
 class TestEnv(AirSimDroneEnv):
     def __init__(self, ip_address, image_shape, env_config):
+        self.eps_n = 0
         super(TestEnv, self).__init__(ip_address, image_shape, env_config)
+        self.agent_traveled = []
         self.random_start = False
 
     def setup_flight(self):
         super(TestEnv, self).setup_flight()
+        self.eps_n += 1
 
         # Start the agent at a random yz position
-        y_pos = (np.random.rand()-0.5)*1
-        z_pos = (np.random.rand()-0.5)*1
+        y_pos, z_pos = (0,0)
         pose = airsim.Pose(airsim.Vector3r(self.agent_start_pos,y_pos,z_pos))
         self.drone.simSetVehiclePose(pose=pose, ignore_collision=True)
         
-        # Get target distance for reward calculation
-        self.target_dist_prev = np.linalg.norm(np.array([y_pos, z_pos]) - self.target_pos)
+    def compute_reward(self):
+        reward = 0
+        done = 0
+
+        x,_,_ = self.drone.simGetVehiclePose().position
+
+        if self.is_collision():
+            done = 1
+            self.agent_traveled.append(x)
+    
+        if done and self.eps_n % 5 == 0:
+            print("---------------------------------")
+            print("> Total episodes:", self.eps_n)
+            print("> Flight distance (mean): %.2f" % (np.mean(self.agent_traveled)))
+            print("> Holes reached (max):", int(np.max(self.agent_traveled)//4))
+            print("> Holes reached (mean):", int(np.mean(self.agent_traveled)//4))
+            print("---------------------------------\n")
+        
+        return reward, done
