b17398d93f1d198dd26895468e07e0ed7ba41e25
Bilal Kabas <bilalkabas@icloud.com>
2021-09-22 19:50:41 +0300

    Create policy_run.py

---
+++ policy_run.py
@@ -0,0 +1,36 @@
+import gym
+import time
+import yaml
+
+from stable_baselines3 import DQN, PPO
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import DummyVecEnv, VecTransposeImage
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.callbacks import EvalCallback
+from scripts.network import CNN, CNN2, ResNet18, BasicCNN
+
+# Get train environment configs
+with open('scripts/config.yml', 'r') as f:
+    env_config = yaml.safe_load(f)
+
+# Create a DummyVecEnv
+env = DummyVecEnv([lambda: Monitor(
+    gym.make(
+        "scripts:test-env-v0", 
+        ip_address="127.0.0.1", 
+        image_shape=(50,50,3),
+        env_config=env_config["TrainEnv"]
+    )
+)])
+
+# Wrap env as VecTransposeImage (Channel last to channel first)
+env = VecTransposeImage(env)
+
+# Load an existing model
+model = PPO.load(env=env, path="\saved_policy\ppo_navigation_policy")
+
+# Run the trained policy
+obs = env.reset()
+for i in range(2000):
+    action, _ = model.predict(obs, deterministic=True)
+    obs, rewards, dones, info = env.step(action)
